156124193de2bfb855a0443a1ebbc6206066b0f7
stephen-1bee <joaofelixo764@gmail.com>
2024-03-20 17:38:17 +0000

    few changes

--- utils/db.js
+++ utils/db.js
@@ -2,8 +2,21 @@ const mongoose = require("mongoose")
 
 const con = async (req, res) => {
   try {
-    await mongoose.connect(process.env.DB_STRING)
-    console.log("dev db connection established")
+    await mongoose.connect(
+      process.env.ROOM === "prod"
+        ? process.env.PROD_STRING
+        : process.env.ROOM === "dev"
+        ? process.env.DEV_STRING
+        : null
+    )
+
+    if (process.env.ROOM === "dev") {
+      console.log("dev db connection established")
+    } else if (process.env.PROD_STRING) {
+      console.log("prod db connection established")
+    } else {
+      console.log("invalid db connection")
+    }
   } catch (err) {
     console.log(err)
   }
